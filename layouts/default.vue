c89a80e7dffb313213f068da00a5dffb4945240b
SzlomoGoldstein <gadulecki@gmail.com>
2024-04-09 20:05:20 +0200

    podstawowy layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
-    <VApp>
+  <v-app id="inspire">
+    <v-app-bar>
+      <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>.NET Mentor - Page Monitor</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+
+    <v-main>
+      <div class="pa-4">
         <NuxtPage />
-    </VApp>
+      </div>
+    </v-main>
+  </v-app>
 </template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay();
+
+const drawer = ref(false)
+</script>

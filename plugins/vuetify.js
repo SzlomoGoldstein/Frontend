c3ef3daf2c943719e7ed52b1940e83afa5ed52c9
SzlomoGoldstein <gadulecki@gmail.com>
2024-04-17 19:51:49 +0200

    własne ustawienia - motywy

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,12 +1,32 @@
 // import this after install `@mdi/font` package
 import '@mdi/font/css/materialdesignicons.css'
-
+import colors from 'vuetify/util/colors'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    defaults: {
+      global: {
+      },
+      VList: {
+      },
+    },
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light:{
+          colors: {
+            'brand': colors.purple.lighten4,
+          }
+        },
+        dark: {
+          colors: {
+            'brand': colors.purple.lighten4,
+          }
+        }
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
